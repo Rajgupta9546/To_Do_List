4478a980a84d0682c6df43b185c2308c3d3f3bab
Raj Gupta <154043831+Rajgupta9546@users.noreply.github.com>
2026-02-27 17:51:40 +0530

    Created basic Todo UI
    Created basic Todo UI

---
+++ App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>My To-Do List</h1>
+
+      <input type="text" placeholder="Enter task..." />
+      <button>Add</button>
+
+      <ul></ul>
+    </div>
+  );
+}
+
+export default App;
